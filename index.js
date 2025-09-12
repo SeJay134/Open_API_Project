65734574c2cd6a81bc545d1bcec1826d6c2e8a29
SeJay134 <patrushev.s.job@gmail.com>
2025-09-12 15:30:19 -0400

    added js html css added fetch

---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+https://open-meteo.com/en/docs?hourly=temperature_2m,weather_code
+*/
+/*
+https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,weather_code
+*/
+
+
+fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,weather_code')
+    .then(urlapiweather => {
+        console.log("urlapiweather", urlapiweather);
+        return urlapiweather.json();
+    })
+    .catch(error => {
+        console.log("error", error);
+        return error;
+    })
